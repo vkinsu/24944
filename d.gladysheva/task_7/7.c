6509bc9f14cc303eafec27267c88829fe0dfd33a
Korzh1637 <d.gladysheva@g.nsu.ru>
2025-09-30 00:30:37 +0700

    Update 7.c

--- d.gladysheva/task_7/7.c
+++ d.gladysheva/task_7/7.c
@@ -17,7 +17,7 @@ int main()
     fstat(st, &file_info);
 
     int file_size = file_info.st_size;
-    char* text = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, st, 0);
+    char* text = mmap(NULL, file_size, 1, MAP_PRIVATE, st, 0);
 
     int count_str = 0;
     for (int i = 0; i < file_size; i++) { if (text[i] == '\n') { count_str++;} }
@@ -38,21 +38,27 @@ int main()
     pos_str[count_str] = file_size;
 
     signal(SIGALRM, timeout_handler);
-    alarm(5);
 
-    printf("Введите номер строки (1-%d): ", count_str);
+    while(1)
+    {
+        alarm(5);
 
-    int n;
-    int result = scanf("%d", &n);
-    
-    if (result == 1)
-    {
-        alarm(0);
-        for (int i = pos_str[n-1]; i < pos_str[n]; i++) { printf("%c", text[i]); }
-    }
-    else
-    {
-        for (int i = 0; i < file_size; i++) { printf("%c", text[i]); }
+        printf("Введите номер строки (1-%d): ", count_str);
+
+        int n;
+        int result = scanf("%d", &n);
+
+        if (n == 0) { break; }
+        
+        if (result == 1)
+        {
+            alarm(0);
+            for (int i = pos_str[n-1]; i < pos_str[n]; i++) { printf("%c", text[i]); }
+        }
+        else
+        {
+            for (int i = 0; i < file_size; i++) { printf("%c", text[i]); }
+        }
     }
 
     munmap(text, file_info.st_size);
