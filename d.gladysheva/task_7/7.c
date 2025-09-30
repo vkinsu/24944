78b4564c0433413fe733ceb5fedac818a5768f2a
Korzh1637 <d.gladysheva@g.nsu.ru>
2025-09-30 17:18:27 +0700

    Update 7.c

--- d.gladysheva/task_7/7.c
+++ d.gladysheva/task_7/7.c
@@ -17,7 +17,7 @@ int main()
     fstat(st, &file_info);
 
     int file_size = file_info.st_size;
-    char* text = mmap(NULL, file_size, 1, MAP_PRIVATE, st, 0);
+    char* text = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, st, 0);
 
     int count_str = 0;
     for (int i = 0; i < file_size; i++) { if (text[i] == '\n') { count_str++;} }
@@ -36,6 +36,16 @@ int main()
     }
 
     pos_str[count_str] = file_size;
+
+    printf("== Таблица соответствия строки и номера ==\n");
+    for (int j = 0; j < count_str; j++)
+    {
+        printf("%d -> ", j+1);
+
+        for (int i = pos_str[j]; i < pos_str[j+1]; i++) { printf("%c", text[i]); }
+
+        printf("\n");
+    }
 
     signal(SIGALRM, timeout_handler);
 
