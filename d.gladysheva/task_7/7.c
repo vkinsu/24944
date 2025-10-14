bfe4cb0e557d56e2c752cf9e332062d1459900db
Korzh1637 <d.gladysheva@g.nsu.ru>
2025-10-14 11:46:28 +0700

    Update 7.c

--- d.gladysheva/task_7/7.c
+++ d.gladysheva/task_7/7.c
@@ -14,17 +14,28 @@ int main()
 {
     int st = open("input.txt", O_RDONLY);
     struct stat file_info;
-    fstat(st, &file_info);
+    fstat(st, &file_info); //получение информации о файле
 
-    int file_size = file_info.st_size;
+    int file_size = file_info.st_size; //размер в байтах
     char* text = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, st, 0);
 
     int count_str = 0;
     for (int i = 0; i < file_size; i++) { if (text[i] == '\n') { count_str++;} }
 
-    int pos_str[++count_str];
+    if (text[file_size - 1] != '\n') { count_str++; }
+
+    int pos_str[count_str + 1];
     pos_str[0] = 0;
     int idx = 1;
+
+    for (int i = 0; i < file_size; i++)
+    {
+        if (text[i] == '\n') 
+        {
+            pos_str[idx] = i + 1;
+            idx++;
+        }
+    }
 
     for (int i = 0; i < file_size; i++)
     {
@@ -40,34 +51,56 @@ int main()
     printf("== Таблица соответствия строки и номера ==\n");
     for (int j = 0; j < count_str; j++)
     {
-        printf("%d -> ", j+1);
-
-        for (int i = pos_str[j]; i < pos_str[j+1]; i++) { printf("%c", text[i]); }
-
+        printf("%d	%d	%d", j+1, pos_str[j], pos_str[j+1] - pos_str[j]);
         printf("\n");
     }
 
     signal(SIGALRM, timeout_handler);
+    int flag = 0;
 
     while(1)
     {
-        alarm(5);
-
         printf("Введите номер строки (1-%d): ", count_str);
+	fflush(stdout);
+
+	if (flag == 0)
+	{
+		timeout = false;
+        	alarm(5);
+		flag =1;
+	}	
 
         int n;
         int result = scanf("%d", &n);
 
+	alarm(0);
+	
+	if (timeout) {
+            printf("\nВывод всего файла:\n");
+            for (int i = 0; i < file_size; i++) { printf("%c", text[i]); }
+	    printf("\n");
+            break;
+        }
+	
+	
+
+	if (result != 1)
+	{
+		printf("Invalid index2\n");
+		int c;
+        	while ((c = getchar()) != '\n' && c != EOF);
+		continue;
+	}
+
         if (n == 0) { break; }
-        
+	else if (n > count_str || n < 1) { printf("Invalid index\n"); continue; 
+		
+	}
+
         if (result == 1)
         {
-            alarm(0);
             for (int i = pos_str[n-1]; i < pos_str[n]; i++) { printf("%c", text[i]); }
-        }
-        else
-        {
-            for (int i = 0; i < file_size; i++) { printf("%c", text[i]); }
+	    printf("\n");
         }
     }
 
